1670a3da11cc35505f2809e2574d041d70d10d46
Samantha Crespo <samkcrespo@gmail.com>
2022-05-02 18:32:56 -0700

    commit tree af8caeb7ad0b66288b1c96f33b17bfd37798aa84

--- src/components/App.js
+++ src/components/App.js
@@ -1,1 +1,20 @@
-// create your App component here
+import React, { useEffect, useState } from "react";
+
+function App() {
+  const [dogData, setDogData] = useState(null);
+  // const [isLoaded, setIsLoaded] = useState(false);
+
+  useEffect(() => {
+    fetch("https://dog.ceo/api/breeds/image/random")
+      .then((res) => res.json())
+      .then((data) => {
+        setDogData(data.message);
+      });
+  }, []);
+
+  if (!dogData) return <p>Loading...</p>;
+
+  return <img src={dogData} alt="A Random Dog" />;
+}
+
+export default App; // create your App component here
